075da3c34e042df318a60af800732f2671726be4
maythushar-music <maythusharofficial@gmail.com>
2025-03-02 17:01:37 +0630

    Add files via upload

---
+++ maythusharmusic/plugins/sudo/logger.py
@@ -0,0 +1,25 @@
+
+import config
+from strings import command
+from maythusharmusic import app
+from maythusharmusic.misc import SUDOERS
+from maythusharmusic.utils.database import add_off, add_on
+from maythusharmusic.utils.decorators.language import language
+
+
+@app.on_message(command("LOGGER_COMMAND") & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip()
+    state = state.lower()
+    if state == "enable":
+        await add_on(config.LOG)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(config.LOG)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
